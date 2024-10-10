c99cf1219948e4f4b2cd1032cb6e362fef8ea698
Hajin-Bang <hajin0619@naver.com>
2024-10-10 10:25:37 +0900

    [Refactor] - Purchase Redux

---
+++ src/store/purchase/purchaseStore.ts
@@ -0,0 +1,34 @@
+import { create } from 'zustand';
+
+interface PurchaseState {
+  isLoading: boolean;
+  error: string | null;
+  purchaseStart: () => void;
+  purchaseSuccess: () => void;
+  purchaseFailure: (error: string) => void;
+}
+
+const usePurchaseStore = create<PurchaseState>((set) => ({
+  isLoading: false,
+  error: null,
+
+  purchaseStart: () =>
+    set(() => ({
+      isLoading: true,
+      error: null,
+    })),
+
+  purchaseSuccess: () =>
+    set(() => ({
+      isLoading: false,
+      error: null,
+    })),
+
+  purchaseFailure: (error) =>
+    set(() => ({
+      isLoading: false,
+      error,
+    })),
+}));
+
+export default usePurchaseStore;
