71761268060f243aa630f84b53908021e60a910e
Hajin-Bang <hajin0619@naver.com>
2024-10-11 14:35:15 +0900

    [Feat] - Reat.lazy, Suspense

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,27 @@
 import { createBrowserRouter, Outlet } from 'react-router-dom';
-
 import { pageRoutes } from '@/apiRoutes';
-import { Cart } from '@/pages/cart';
+import React, { Suspense } from 'react';
+
+const Cart = React.lazy(() =>
+  import('@/pages/cart').then((module) => ({ default: module.Cart }))
+);
+const LoginPage = React.lazy(() =>
+  import('@/pages/login').then((module) => ({ default: module.LoginPage }))
+);
+const Purchase = React.lazy(() =>
+  import('@/pages/purchase').then((module) => ({ default: module.Purchase }))
+);
+const RegisterPage = React.lazy(() =>
+  import('@/pages/register').then((module) => ({
+    default: module.RegisterPage,
+  }))
+);
+
+import { Home } from '@/pages/home';
 import { RootErrorBoundary } from '@/pages/common/components/RootErrorHandler';
 import { RootSuspense } from '@/pages/common/components/RootSuspense';
 import { ErrorPage } from '@/pages/error/components/ErrorPage';
 import { NotFoundPage } from '@/pages/error/components/NotFoundPage';
-import { Home } from '@/pages/home';
-import { LoginPage } from '@/pages/login';
-import { Purchase } from '@/pages/purchase';
-import { RegisterPage } from '@/pages/register';
 
 const CommonLayout = () => (
   <RootErrorBoundary>
@@ -23,25 +35,45 @@ const router = createBrowserRouter([
   {
     element: <CommonLayout />,
     children: [
-      { path: pageRoutes.main, element: <Home />, errorElement: <ErrorPage /> },
+      {
+        path: pageRoutes.main,
+        element: <Home />,
+        errorElement: <ErrorPage />,
+      },
       {
         path: pageRoutes.register,
-        element: <RegisterPage />,
+        element: (
+          <Suspense fallback={<div>로딩 중...</div>}>
+            <RegisterPage />
+          </Suspense>
+        ),
         errorElement: <ErrorPage />,
       },
       {
         path: pageRoutes.login,
-        element: <LoginPage />,
+        element: (
+          <Suspense fallback={<div>로딩 중...</div>}>
+            <LoginPage />
+          </Suspense>
+        ),
         errorElement: <ErrorPage />,
       },
       {
         path: pageRoutes.cart,
-        element: <Cart />,
+        element: (
+          <Suspense fallback={<div>로딩 중...</div>}>
+            <Cart />
+          </Suspense>
+        ),
         errorElement: <ErrorPage />,
       },
       {
         path: pageRoutes.purchase,
-        element: <Purchase />,
+        element: (
+          <Suspense fallback={<div>로딩 중...</div>}>
+            <Purchase />
+          </Suspense>
+        ),
         errorElement: <ErrorPage />,
       },
       { path: '*', element: <NotFoundPage /> },
